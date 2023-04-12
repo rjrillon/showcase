d3d2c3bfb89fa824a3fe52106828a4c9ff2860f1
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-12 16:36:48 -0700

    user and post routes setup

---
+++ server/controllers/users.js
@@ -0,0 +1,65 @@
+import User from "../models/User.js";
+
+// READ
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map(id => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// UPDATE
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    //if friend is already part of main users friend id, then remove it
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter(id => id !== friendId); //copies same arr anytime id !== friendId
+      friend.friends = friend.friends.filter(id => id !== id);
+    } else {
+      // if not already a friend, then friend. do the same for the person being added
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map(id => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
