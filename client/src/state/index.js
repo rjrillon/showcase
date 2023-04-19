7cddbc95e46098d3d6bc59de0f8fca71c6d47b8b
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-19 02:49:26 -0700

    states

---
+++ client/src/state/index.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// this state will be stored in our global state
+// data will be accessible from anywhere in our app and can grab it from anywhere we want
+// don't have to pass in state and props down to different components
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: state => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: state => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends does not exist :(");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map(post => {
+        //only return the relevant post that we intend to update
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
