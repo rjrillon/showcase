771e5bb8349fb9649d72fdd45886c7db46b417ed
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-23 10:57:38 -0700

    login

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,36 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1500px");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 5%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="40px" color="primary">
+          showcase
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="700" variant="h6" sx={{ mb: "1rem" }}>
+          Welcome to showcase! Share and connect with friends today!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
+
 export default LoginPage;
