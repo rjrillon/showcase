6da82a6cbc99986fa1c36b427377daf940d84402
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-25 09:18:36 -0700

    more register work :'(

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
 
 // Yup Validation Schema:
 // Will determine the shape of how the form library is going to be saving this information
@@ -40,17 +42,51 @@ const initialValuesLogin = {
 const Form = () => {
   const [pageType, setPageType] = useState("login");
   const { palette } = useTheme();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const register = async values => {
-    try {
-      const response = await axios.post("/auth/register", values);
-      console.log(response);
-      navigate("/login");
-    } catch (error) {
-      console.log(error);
+  const register = async (values, onSubmitProps) => {
+    // normally we can just use values and pass it to a normal request body itself,
+    // but because we have a picture image, we must use formData.
+    // this allows us to send form info with image
+    const formData = new FormData();
+
+    // loop through every key, value in values object and append to formData
+    // unconventional, but when u have an img this is one way to send the image through the request body
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturePath", values.picture.name);
+  };
+
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+    const loggedIn = await loggedInResponse.json();
+    onSubmitProps.resetForm();
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      );
+      navigate("/home");
     }
   };
+
+  return (
+    <Formik
+      onSubmit={}
+      initialValues={}
+      validationSchema={}
+    >
+      
+    </Formik>
+  )
 };
 
 export default Form;
