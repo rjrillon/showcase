7e1242c1e4a1d392a9d40705f6c5c4f89c64ce56
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-25 00:08:43 -0700

    merged dev here and did more register work

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Formik } from "formik";
 import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
 
 // Yup Validation Schema:
 // Will determine the shape of how the form library is going to be saving this information
@@ -16,6 +17,30 @@ const registerSchema = yup.object().shape({
   picture: yup.string().required("required"),
 });
 
-const Form = () => {};
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
+
+const Form = () => {
+  const [pageType, setPageType] = useState("login");
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+};
 
 export default Form;
