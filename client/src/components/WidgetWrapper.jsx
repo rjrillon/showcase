3a79e53d672054f9b08c690dd448c56e4b5cb05d
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-28 12:30:52 -0700

    fixed errors n started working on widgets for homepage

---
+++ client/src/components/WidgetWrapper.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const WidgetWrapper = styled(Box)(({ theme }) => ({
+  padding: "1rem 1rem 0.75rem 1rem",
+  backgroundColor: theme.palette.background.alt,
+  borderRadius: "1rem",
+}));
+
+export default WidgetWrapper;
---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const token = useSelector(state => state.token);
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const data = await response.json();
+    setUser(data);
+  };
+
+  if (!user) {
+    return null;
+  }
+
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return <WidgetWrapper></WidgetWrapper>;
+};
+
+export default UserWidget;
