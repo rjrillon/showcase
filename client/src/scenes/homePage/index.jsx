a5e37b523966e39c42caa9dd4b2db3e98b0f6ce7
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-29 10:50:05 -0700

    first home commit

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,17 +1,39 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
-import state from "state";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const { _id, picturePath } = useSelector(state => state.user);
 
   return (
     <Box>
       <Navbar />
-      <Box width="100%"></Box>
+      <Box
+        width="100%"
+        padding="2rem 5%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="1rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "35%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "50%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+
+        {isNonMobileScreens && (
+          <Box flexBasis="35%">
+            <Box m="2rem" />
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 };
+
 export default HomePage;
