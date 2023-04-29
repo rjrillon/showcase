d0246404fb0574147adc7c22dd18e26579db8652
ROBIN RILLON <rjrillon97@gmail.com>
2023-04-29 12:01:13 -0700

    before userwidget, mypostwidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,9 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
-import state from "state";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("min-width: 1000px");
@@ -10,7 +12,25 @@ const HomePage = () => {
   return (
     <Box>
       <Navbar />
-      <Box width="100%"></Box>
+      <Box
+        width="100%"
+        padding="2rem 5%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="1rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "30%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "50%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+        {isNonMobileScreens && <Box flexBasis="30%"></Box>}
+      </Box>
     </Box>
   );
 };
