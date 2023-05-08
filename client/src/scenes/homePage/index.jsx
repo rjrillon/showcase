8a38550eb282caf08052d741c07d56b61dbb1344
ROBIN RILLON <rjrillon97@gmail.com>
2023-05-07 19:42:35 -0700

    postwidget done and added to home with fixed styling

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,10 +3,10 @@ import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
-// import PostsWidget from "scenes/widgets/PostsWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery("min-width: 1000px");
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const { _id, picturePath } = useSelector(state => state.user);
 
   return (
@@ -14,24 +14,30 @@ const HomePage = () => {
       <Navbar />
       <Box
         width="100%"
-        padding="2rem 5%"
+        padding="2rem 6%"
         display={isNonMobileScreens ? "flex" : "block"}
-        gap="1rem"
+        gap="0.5rem"
         justifyContent="space-between"
       >
-        <Box flexBasis={isNonMobileScreens ? "30%" : undefined}>
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
 
         <Box
-          flexBasis={isNonMobileScreens ? "50%" : undefined}
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis="30%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
       </Box>
     </Box>
   );
 };
+
 export default HomePage;
--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -23,8 +23,12 @@ const PostWidget = ({
   likes,
   comments,
 }) => {
+  const [isComments, setIsComments] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector(state => state.token);
+  const loggedInUserId = useSelector(state => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
 
   const { palette } = useTheme();
   const main = palette.neutral.main;
@@ -44,30 +48,63 @@ const PostWidget = ({
   };
 
   return (
-    <WidgetWrapper>
+    <WidgetWrapper m="2rem 0">
       <Friend
         friendId={postUserId}
         name={name}
         subtitle={location}
         userPicturePath={userPicturePath}
       />
-      <Typography color={main}>{description}</Typography>
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
       {picturePath && (
-        <img src={`http://localhost:3001/assets/${picturePath}`} />
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${picturePath}`}
+        />
       )}
-      <FlexBetween>
-        <FlexBetween>
-          <FlexBetween>
-            <IconButton></IconButton>
+      <FlexBetween mt="0.25rem">
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={patchLike}>
+              {isLiked ? (
+                <FavoriteOutlined sx={{ color: primary }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              )}
+            </IconButton>
             <Typography>{likeCount}</Typography>
           </FlexBetween>
 
-          <FlexBetween>
-            <IconButton></IconButton>
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={() => setIsComments(!isComments)}>
+              <ChatBubbleOutlineOutlined />
+            </IconButton>
             <Typography>{comments.length}</Typography>
           </FlexBetween>
         </FlexBetween>
+
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
       </FlexBetween>
+      {isComments && (
+        <Box mt="0.5rem">
+          {comments.map((comment, i) => (
+            <Box key={`${name}-${i}`}>
+              <Divider />
+              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                {comment}
+              </Typography>
+            </Box>
+          ))}
+          <Divider />
+        </Box>
+      )}
     </WidgetWrapper>
   );
 };
